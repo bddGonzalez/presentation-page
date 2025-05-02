3a4effb51f6d63613483273909383f60cb27126c
brian <briandedgonzalez@gmail.com>
2025-05-02 14:32:38 -0300

    add netlify adapter to support ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "en",
     // routing: "manual",
   },
+
+  adapter: netlify(),
 });
