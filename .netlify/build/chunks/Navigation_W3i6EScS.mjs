13a4a5a9b8980b527e30fd29e1028e59acc67ac2
brian <briandedgonzalez@gmail.com>
2025-05-07 15:27:35 -0300

    add spanish highlighting for index page

--- .netlify/build/chunks/Navigation_W3i6EScS.mjs
+++
@@ -1,4 +0,0 @@
-import './astro/server_Z1Ea7FKf.mjs';
-import 'kleur/colors';
-export { $ as default, a as file, b as url } from './Navigation_DAcQ6g5g.mjs';
-/* empty css                         */
